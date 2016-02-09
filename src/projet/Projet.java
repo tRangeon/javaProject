50c308ef2a45415504bcbd26c295479a4395fa9a
tRangeon <thomas.rangeon@isen-lille.fr>
2016-02-09 15:44:56 +0100

    Commit V5.1

--- src/projet/Projet.java
+++ src/projet/Projet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projet;
 
 import java.util.*;
@@ -122,7 +117,7 @@ public class Projet {
         p3.ajouterObjet(biere);
         p5.ajouterObjet(badge);
         
-                
+        // Emplacement de depart du joueur    
         joueur.setPosition(p1);
 
         while( (maitreDuJeu.getVictoire() != 0) && (maitreDuJeu.getVictoire() != 1) ){
